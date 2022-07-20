e1b626739519ee7af8c97597293a476251c1f687
jhewitt11 <jbh9592@gmail.com>
2022-07-20 12:28:43 -0400

    Added web interface
    Added
    .html and .css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
 import json
 
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, jsonify, request, flash
 from newspaper import Article
 
 app = Flask(__name__)
-
+app.secret_key = "chauncey_billups_lasagna_turkey"
 
 
 def get_summary(url):
@@ -26,20 +26,32 @@ def get_summary(url):
     
     return title, summary
 
+@app.route("/hello")
+def index():
+    return render_template("index.html")
 
-@app.route('/summarize', methods = ['POST'])
+
+@app.route('/summarize', methods = ['POST', 'GET'])
 def summarize():
-    if request.method == 'POST':
-        f = request.files['file']       
-        
-        url = f.read()
-        url = url.decode('utf-8')
 
-        TITLE, SUMMARY = get_summary(url)
+    url = str(request.form['link_input'])
+    TITLE, SUMMARY = get_summary(url)
      
-        return jsonify({'article_title': TITLE, 'article_summary' : SUMMARY})
+    flash("Title : " + TITLE)
+    flash("Summary : " + SUMMARY)
     
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
     
+'''
+# for txt file input
+    if request.method == 'POST':
+        f = request.files['file']       
+        url = f.read()
+        url = url.decode('utf-8')
+'''
+
+#for txt file input    
+#    return jsonify({'article_title': TITLE, 'article_summary' : SUMMARY})
